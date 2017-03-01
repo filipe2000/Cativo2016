5df577b3b1baa70704822f3ee148cc5383f1059e
Filipe <filipeosouza2000@hotmail.com>
2017-03-01 13:28:03 -0300

    raiz
    inicio

---
+++ catalogo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+ <html class="no-js" lang="pt-br"> 
+   
+    <head>
+    <title>Cativo Comercial e Logística</title>	
+    <?php include 'meta.php' ?>
+    </head>
+<body>
+<?php include "header.php" ?> 
+<div class="container">		
+	
+	<div class="row subR ">						
+						
+	<div id="cat" class="col-md-10 col-md-offset-2 col-sm-12 col-xs-12 ">	
+		 <a href="catalogo/catalogoCativo.pdf" target="blank"> 
+		 <img src="img/capa.jpg">
+		 </a>	
+		 <script>
+		 /*
+		 $(window).load(function(){
+		  $('#cat')
+		    .html('<iframe src="includes/catalogo/cat.html" class="flipPdf"></iframe>');
+		});*/
+		 </script>
+		 
+	</div>
+	
+	
+	</div>
+	
+</div>					
+</body>   
+<section id="foot" class="bg-f">
+	<?php include ('footer.php');  ?>			
+</section>
+<?php include "metaJS.php";  ?>
+</html>
